cb203bf283c7d651d255be56529f7fd623475ae4
unknown <m.rudranil@googlemail.com>
2021-03-07 21:17:09 -0500

    Initial commit

---
+++ src/test/java/com/optionsorchestrator/OptionsStreamOrchestratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.optionsorchestrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptionsStreamOrchestratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
